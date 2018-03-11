4b2701286370bec06429761e729e449c062f183a
Trent Fowler <trenttfowler@gmail.com>
2018-03-11 17:51:56 +0100

    Added Board.java to hold game state.

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -1,4 +1,110 @@
 package tetris;
 
+import java.awt.*;
+
 public class Board {
+
+    private Color[][] squares;
+
+    public Board() {
+        squares = new Color[20][10];
+        init();
+    }
+
+    public Color[][] get() { return squares; }
+
+    private void init() {
+        for (int row = 0; row < 20; row++)
+            for (int col = 0; col < 10; col++)
+                // Gray represents an empty square.
+                squares[row][col] = Color.DARK_GRAY;
+    }
+
+    private boolean is_occupied(Point point) {
+        return squares[point.y][point.x] == Color.DARK_GRAY ? false: true;
+    }
+
+    public boolean can_move(Piece piece, Tetris.Move direction) {
+        if (direction == Tetris.Move.LEFT)
+            for (Point xy_index: piece.occupies())
+                if (xy_index.x == 0 || this.is_occupied(
+                        new Point(xy_index.x - 1, xy_index.y)
+                ))
+                    return false;
+
+        if (direction == Tetris.Move.RIGHT)
+            for (Point xy_index: piece.occupies())
+                if (xy_index.x == 9 || this.is_occupied(
+                        new Point(xy_index.x + 1, xy_index.y)
+                ))
+                    return false;
+
+        if (direction == Tetris.Move.DOWN)
+            for (Point xy_index: piece.occupies())
+                if (xy_index.y == 19 || this.is_occupied(
+                        new Point(xy_index.x, xy_index.y + 1)
+                ))
+                    return false;
+
+        if (direction == Tetris.Move.ROTATE) { }
+            //TODO: Implement check for rotate.
+
+        return true;
+    }
+
+    public void add(Piece piece) {
+        for (Point xy_index: piece.occupies())
+            if (xy_index.x >= 0 && xy_index.x < 10
+                    && xy_index.y >= 0 && xy_index.y < 20)
+                squares[xy_index.y][xy_index.x] = piece.color();
+    }
+
+    public boolean did_hit_bottom(Piece piece) {
+        for (Point xy_index: piece.occupies())
+            if (xy_index.y == 19 || this.is_occupied(
+                    new Point(xy_index.x, xy_index.y + 1)
+            ))
+                return true;
+
+        return false;
+    }
+
+    /**
+     * Draws the game board showing the position
+     * of the pieces, like:
+     *
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * ----------
+     * --X-------
+     * --XXXXXX--
+     * XXXXXXXXX-
+     * XXXXXXXXX-
+     * XXXXXXXXX-
+     * XXXXXXXXXX
+     * XXXXXXXXXX
+     * XXXXXXXXXX
+     * XXXXXXXXXX
+     * XXXXXXXXXX
+     *
+     * @return
+     */
+    @Override public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int row = 0; row < 20; row++) {
+            for (int col = 0; col < 10; col++) {
+                s = squares[row][col] == Color.DARK_GRAY ?
+                        s.append('-'): s.append('X');
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 }
